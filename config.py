7c3088987d00fc34100c2985cbdadd9467abc4c9
etters <essia.etters@gmail.com>
2022-09-01 13:34:46 +0100

    project structure

---
+++ config.py
@@ -0,0 +1,7 @@
+# >>> import os 
+# >>> to get the envirment variable named 'SECRET_KEY' (configuré auparavant sur votre pc)
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+FB_APP_ID = 1200420960103822
